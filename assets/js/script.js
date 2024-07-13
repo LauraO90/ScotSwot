4a0e87a8c3e4c7a35f85ffb670e4eb0a428ee36d
Laura O Donnell <lauraodonnell90@hotmail.com>
2024-07-13 20:29:17 +0000

    Set up quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,120 @@
+const easyQuestions = [
+    {
+        question: 'What does "wee" mean?',
+        options: ['Funny', 'Small', 'Tall'],
+        answer: 'Small',
+    },
+    {
+        question: 'What does "aye" mean?',
+        options: ['Yes', 'Sorry', 'Hello'],
+        answer: 'Yes',
+    },
+    {
+        question: 'What does "naw" mean?',
+        options: ['Please', 'No', 'Bite'],
+        answer: 'No',
+    },
+    {
+        question: 'What does "bonnie" mean?',
+        options: ['Beautiful', 'Water', 'Sweet'],
+        answer: 'Beautiful',
+    },
+    {
+        question: 'What does "braw" mean?',
+        options: ['Cold', 'Handsome', 'Hungry'],
+        answer: 'Handsome',
+    },
+    {
+        question: 'What does "lassie" mean?',
+        options: ['Dog', 'Drink', 'Girl'],
+        answer: 'Girl',
+    },
+    {
+        question: 'What does "bairn" mean?',
+        options: ['Child', 'Pet', 'Home'],
+        answer: 'Child',
+    },
+]
+
+const medQuestions = [
+{
+    question: 'What does "wheesht" mean?',
+    options: ['Be Quiet', 'Go Away', 'Stop It'],
+    answer: 'Be Quiet',
+},
+{
+    question: 'What does "coorie" mean?',
+    options: ['Cuddle', 'Mountain', 'Sneeze'],
+    answer: 'Cuddle',
+},
+{
+    question: 'What does "gallus" mean?',
+    options: ['Expensive', 'Bold', 'Tired'],
+    answer: 'Bold',
+},
+{
+    question: 'What does "crabbit" mean?',
+    options: ['Grumpy', 'Wet', 'Tasty'],
+    answer: 'Grumpy',
+},
+{
+    question: 'What does "numpty" mean?',
+    options: ['Young', 'Fool', 'Strange'],
+    answer: 'Fool',
+},
+{
+    question: 'What does "boggin" mean?',
+    options: ['Dirty', 'Pleasant', 'Garden'],
+    answer: 'Dirty',
+},
+{
+    question: 'What does "hoachin" mean?',
+    options: ['Busy', 'Hot', 'Shopping'],
+    answer: 'Busy',
+},
+
+]
+
+const hardQuestions = [
+{
+    question: 'What does "hee-haw" mean?',
+    options: ['Angry', 'Party', 'Nothing'],
+    answer: 'Nothing',
+},
+{
+    question: 'What does "sleekit" mean?',
+    options: ['Sly', 'Scary', 'Bug'],
+    answer: 'Sly',
+},
+{
+    question: 'What does "dreich" mean?',
+    options: ['Silly', 'Dreary', 'Happy'],
+    answer: 'Dreary',
+},
+{
+    question: 'What does "patch" mean?',
+    options: ['Abandon', 'Repair', 'Friend'],
+    answer: 'Abandon',
+},
+{
+    question: 'What does "scunnered" mean?',
+    options: ['Sporty', 'Disappointed', 'Excited'],
+    answer: 'Disappointed',
+},
+{
+    question: 'What does "winch" mean?',
+    options: ['Flinch', 'Kiss', 'Ignore'],
+    answer: 'Kiss',
+},
+{
+    question: 'What does "lugs" mean?',
+    options: ['Legs', 'Ears', 'Arms'],
+    answer: 'Ears',
+},
+]
+
+
+
+
+
+
