bf53f8ecd553f82641e65650794351bd1ed711d0
Laura O Donnell <lauraodonnell90@hotmail.com>
2024-07-18 22:02:50 +0000

    Create separate javascript file for quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,3 @@
-const easyQuestions = [
-    {
-        question: 'What does "wee" mean?',
-        options: ['Funny', 'Small', 'Tall'],
-        answer: 'Small',
-    },
-    {
-        question: 'What does "aye" mean?',
-        options: ['Yes', 'Sorry', 'Hello'],
-        answer: 'Yes',
-    },
-    {
-        question: 'What does "naw" mean?',
-        options: ['Please', 'No', 'Bite'],
-        answer: 'No',
-    },
-    {
-        question: 'What does "bonnie" mean?',
-        options: ['Beautiful', 'Water', 'Sweet'],
-        answer: 'Beautiful',
-    },
-    {
-        question: 'What does "braw" mean?',
-        options: ['Cold', 'Handsome', 'Hungry'],
-        answer: 'Handsome',
-    },
-    {
-        question: 'What does "lassie" mean?',
-        options: ['Dog', 'Drink', 'Girl'],
-        answer: 'Girl',
-    },
-    {
-        question: 'What does "bairn" mean?',
-        options: ['Child', 'Pet', 'Home'],
-        answer: 'Child',
-    },
-]
-
-const mediumQuestions = [
-    {
-        question: 'What does "wheesht" mean?',
-        options: ['Be Quiet', 'Go Away', 'Stop It'],
-        answer: 'Be Quiet',
-    },
-    {
-        question: 'What does "coorie" mean?',
-        options: ['Cuddle', 'Mountain', 'Sneeze'],
-        answer: 'Cuddle',
-    },
-    {
-        question: 'What does "gallus" mean?',
-        options: ['Expensive', 'Bold', 'Tired'],
-        answer: 'Bold',
-    },
-    {
-        question: 'What does "crabbit" mean?',
-        options: ['Grumpy', 'Wet', 'Tasty'],
-        answer: 'Grumpy',
-    },
-    {
-        question: 'What does "numpty" mean?',
-        options: ['Young', 'Fool', 'Odd'],
-        answer: 'Fool',
-    },
-    {
-        question: 'What does "boggin" mean?',
-        options: ['Dirty', 'Pleasant', 'Garden'],
-        answer: 'Dirty',
-    },
-    {
-        question: 'What does "hoachin" mean?',
-        options: ['Busy', 'Hot', 'Shopping'],
-        answer: 'Busy',
-    },
-
-]
-
-const hardQuestions = [
-    {
-        question: 'What does "hee-haw" mean?',
-        options: ['Angry', 'Party', 'Nothing'],
-        answer: 'Nothing',
-    },
-    {
-        question: 'What does "sleekit" mean?',
-        options: ['Sly', 'Scary', 'Bug'],
-        answer: 'Sly',
-    },
-    {
-        question: 'What does "dreich" mean?',
-        options: ['Silly', 'Dreary', 'Happy'],
-        answer: 'Dreary',
-    },
-    {
-        question: 'What does "patch" mean?',
-        options: ['Abandon', 'Repair', 'Friend'],
-        answer: 'Abandon',
-    },
-    {
-        question: 'What does "scunnered" mean?',
-        options: ['Sporty', 'Disappointed', 'Excited'],
-        answer: 'Disappointed',
-    },
-    {
-        question: 'What does "winch" mean?',
-        options: ['Flinch', 'Kiss', 'Ignore'],
-        answer: 'Kiss',
-    },
-    {
-        question: 'What does "lugs" mean?',
-        options: ['Legs', 'Ears', 'Arms'],
-        answer: 'Ears',
-    },
-]
-
-
 const quizContainer = document.getElementById('quiz');
 const currentQuestionNumberElement = document.getElementById('current-question-number');
 const totalQuestionsElement = document.getElementById('total-questions');
@@ -174,19 +58,19 @@ function hideNextQuestionButton() {
     nextQuestionButton.className = 'next-question';
 }
 
-function hideAnswerFeedbackElement () {
+function hideAnswerFeedbackElement() {
     answerFeedbackElement.className = 'answer-feedback';
 }
 
 
 function showQuizSummary() {
-    let quizSummaryText; 
+    let quizSummaryText;
 
     if (totalCorrectAnswers === currentQuestions.length) {
         quizSummaryText = totalCorrectAnswers + '/' + currentQuestions.length + ' - Yaldi (woohoo)!';
     }
     else {
-        quizSummaryText = totalCorrectAnswers + '/' + currentQuestions.length + ' - Nae Luck (better luck next time)!' 
+        quizSummaryText = totalCorrectAnswers + '/' + currentQuestions.length + ' - Nae Luck (better luck next time)!'
     }
 
     quizSummaryElement.innerHTML = quizSummaryText;
@@ -205,7 +89,7 @@ function submitAnswer() {
         answerFeedbackElement.innerHTML = 'Incorrect - try again!';
     }
 
-showAnswerFeedbackElement(); 
+    showAnswerFeedbackElement();
 
     if (currentQuestionIndex === (currentQuestions.length - 1)) {
         showQuizSummary();
@@ -234,7 +118,6 @@ function showCurrentQuestion() {
     optionThreeLabelElement.innerHTML = currentQuestion.options[2];
 
     currentQuestionNumberElement.innerHTML = currentQuestionIndex + 1;
-
 }
 
 
